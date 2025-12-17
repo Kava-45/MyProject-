51376cf6b45ea56af92b81cde253e15a4e45de99
Kava-45 <mechtitana@gmail.com>
2025-12-17 14:31:24 +0300

    Initial commit

--- project_bac/database.py
+++ project_bac/database.py
@@ -2,25 +2,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Настройки подключения
+# настройки подключения
 DB_USER = "postgres"
 DB_PASSWORD = "4565"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "fastapi_db"
 
+
+# подключение к базе данных 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Движок SQLAlchemy
+# движок SQLAlchemy
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-# Сессии
+# сессия 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс моделей
+# наследование всех моделей SQLAlchemy
 Base = declarative_base()
 
-# Зависимость для FastAPI
+# зависимость для FastAPI
 def get_db():
     db = SessionLocal()
     try:
